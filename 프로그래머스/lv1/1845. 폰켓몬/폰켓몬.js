ffc0b2d3b9c6ab4af946b208fa45e2aa37787cfe
Lmoti <tv13579@naver.com>
2022-08-07 00:32:05 +0900

    [level 1] Title: 폰켓몬, Time: 0.33 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,6 @@
 function solution(nums) {
-    let result = [...new Set(nums)];
+    let result = new Set(nums);
     let max = nums.length / 2;
     
-    return result.length < max ? result.length : max
+    return result.size < max ? result.size : max
 }
