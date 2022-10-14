a5dd3d6e0628a15f6de2415e177f2c5c531a51db
Lmoti <tv13579@naver.com>
2022-10-15 03:16:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,20 @@
+function solution(s) {
+    let answer = s;
+    const changeNum = { 'zero' : 0,
+                      'one' : 1,
+                      'two' : 2,
+                      'three' : 3,
+                      'four' : 4,
+                      'five' : 5,
+                      'six' : 6,
+                      'seven' : 7,
+                      'eight' : 8,
+                      'nine' : 9};
+    for (let num in changeNum) {
+        if (answer.includes(num)) {
+            let arr = answer.split(num)
+            answer = arr.join(changeNum[num])
+        }
+    }
+    return Number(answer);
+}
