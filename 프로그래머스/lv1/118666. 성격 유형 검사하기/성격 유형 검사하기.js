fbd107ed94839dcb6879dbefc1b30baa13174063
Lmoti <tv13579@naver.com>
2022-10-24 13:52:10 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 3.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,20 @@
+function solution(survey, choices) {
+    let answer = '';
+    const types = ["RT", "CF", "JM", "AN"]
+    const score = {"R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0};
+    
+    for (let i = 0; i < survey.length; i++) {
+        if (choices[i] < 4) {
+            score[survey[i][0]] += Math.abs(choices[i] - 4);
+        } else {
+            score[survey[i][1]] += choices[i] - 4;
+        }
+    }
+    
+    console.log(score)
+    for (let type of types) {
+        score[type[0]] < score[type[1]] ? answer += type[1] : answer += type[0]
+    }
+    
+    return answer;
+}
