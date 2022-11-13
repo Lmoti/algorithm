a3d71126c6b4befbeeb254e697d5fa50a9833ba0
Lmoti <tv13579@naver.com>
2022-11-13 20:40:34 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,6 @@
+function solution(my_string, num1, num2) {
+    let answer = my_string.split('');
+    answer.splice(num1, 1, my_string[num2]);
+    answer.splice(num2, 1, my_string[num1]);
+    return answer.join('');
+}
