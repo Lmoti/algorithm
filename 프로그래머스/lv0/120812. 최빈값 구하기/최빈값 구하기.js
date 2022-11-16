f0fdf9d49bd7025b1e04f6b75fa49361a3e7c458
Lmoti <tv13579@naver.com>
2022-11-16 19:53:20 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,22 +1,18 @@
 function solution(array) {
+    if (array.length === 1) return array[0];
     const object = {};
-    for (let num of array) {
-        if (!object[num]) object[num] = 1;
-        else object[num]++;
-    }
+    array.forEach((n) => {
+        object[n] = ++object[n] || 1;
+    })
     const value = Object.values(object);
     let maxNum = value[0];
     let answer = Object.keys(object)[0];
-    let count = 0;
     for (let i in object) {
         if (object[i] > maxNum) {
             maxNum = object[i];
             answer = i;
         }
     }
-    for (let num of value) {
-        if (maxNum === num) count++;
-        if (count > 1) return -1;
-    }
-    return Number(answer);
+    value.sort((a, b) => b - a);
+    return value[0] === value[1] ? -1 : Number(answer);
 }
