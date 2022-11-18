b0e2f87bcbcafa7d7ffe67b117bf4b414b1f0cfa
Lmoti <tv13579@naver.com>
2022-11-19 01:01:56 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,3 +1,12 @@
 function solution(my_string) {
-    return [...new Set(my_string)].join('')
+    // return [...new Set(my_string)].join('')
+    const str = {};
+    let answer = '';
+    for (let string of my_string.split('')) {
+        if (!str[string]) {
+            str[string] = 1;
+            answer += string;
+        }
+    }
+    return answer;
 }
