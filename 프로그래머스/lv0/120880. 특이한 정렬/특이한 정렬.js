02ee9d766508b59a2de7248fce92bbb2d91d288c
Lmoti <tv13579@naver.com>
2022-11-22 20:23:52 +0900

    [level 0] Title: 특이한 정렬, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,10 +1,8 @@
 function solution(numlist, n) {
-    const arr = [...numlist];
-    arr.sort((a, b) => {
+    return numlist.sort((a, b) => {
         if (Math.abs(a - n) === Math.abs(b - n)) {
             return b - a;
         }
         return Math.abs(a - n) - Math.abs(b - n);
     })
-    return arr;
 }
