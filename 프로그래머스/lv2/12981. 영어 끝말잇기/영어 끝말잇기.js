f0fe713cb64c0cbea482c6b868779e845a8c8484
Lmoti <tv13579@naver.com>
2022-11-23 14:14:41 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.31 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,24 +1,18 @@
 function solution(n, words) {
-    let count = 1;
-    let cycle = 1;
     const said = [words[0]];
-    let breakGame = false;
     for (let i = 1; i < words.length; i++) {
-        count++;
-        if (count % n === 1) {
-            cycle++;
-            count = 1;
-        }
         if (words[i].length === 1 || words[i].length > 50) {
-            breakGame = true;
-            break;
+            let num = (i + 1)% n;
+            if (num === 0) num = n;
+            return [num, Math.ceil((i + 1) / n)]
         }
         if (said[i - 1][said[i - 1].length - 1] === words[i][0] && !said.includes(words[i])) {
             said.push(words[i]);
         } else {
-            breakGame = true;
-            break;
+            let num = (i + 1) % n;
+            if (num === 0) num = n;
+            return [num, Math.ceil((i + 1)/ n)]
         }
     }
-    return breakGame ? [count, cycle] : [0, 0]
+    return [0, 0]
 }
