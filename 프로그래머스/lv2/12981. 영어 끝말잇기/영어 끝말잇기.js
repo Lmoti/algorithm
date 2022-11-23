7cfdba38a2f10f16e42ce8423f410ebde0303da5
Lmoti <tv13579@naver.com>
2022-11-23 14:15:14 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -2,7 +2,7 @@ function solution(n, words) {
     const said = [words[0]];
     for (let i = 1; i < words.length; i++) {
         if (words[i].length === 1 || words[i].length > 50) {
-            let num = (i + 1)% n;
+            let num = (i + 1) % n;
             if (num === 0) num = n;
             return [num, Math.ceil((i + 1) / n)]
         }
@@ -11,7 +11,7 @@ function solution(n, words) {
         } else {
             let num = (i + 1) % n;
             if (num === 0) num = n;
-            return [num, Math.ceil((i + 1)/ n)]
+            return [num, Math.ceil((i + 1) / n)]
         }
     }
     return [0, 0]
