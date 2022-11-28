15a9e826c5353ac08b16c9b0fc80b0e4543d8f4d
Lmoti <tv13579@naver.com>
2022-11-28 14:25:48 +0900

    [level 2] Title: 괄호 변환, Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
@@ -1,41 +1,28 @@
 function solution(p) {
-    let right = 0;
-    for (let i = 0; i < p.length; i++) {
-        if (p[i] === '(') right++;
-        else right--;
-        if (right < 0) break;
-    }
-    if (right === 0) return p;
-    function helper(words) {
-        let right1 = 0;
+        let right = 0;
         let u = '';
         let v = '';
-        for (let i = 0; i < words.length; i++) {
-            if (words[i] === '(') right1++;
-            else right1--;
-            if (right1 < 0) break;
+        for (let i = 0; i < p.length; i++) {
+            p[i] === '(' ? right++ : right--;
+            if (right < 0) break;
         }
-        if (right1 === 0) return words;
-        else right1 = 0;
-        for (let i = 0; i < words.length; i++) {
-            if (words[i] === '(') right1++;
-            else right1--;
-            if (right1 === 0) {
-                u = words.slice(0, i + 1);
-                v = words.slice(i + 1);
+        if (right === 0) return p;
+        else right = 0;
+        for (let i = 0; i < p.length; i++) {
+            p[i] === '(' ? right++ : right--;
+            if (right === 0) {
+                u = p.slice(0, i + 1);
+                v = p.slice(i + 1);
                 break;
             }
         }
-        if (u[0] === '(') return u + helper(v);
+        if (u[0] === '(') return u + solution(v);
         else {
             let a = '';
             let b = u.slice(1, u.length - 1);
             for (let i = 0; i < b.length; i++) {
-                if (b[i] === '(') a += ')';
-                else a += '(';
+                b[i] === '(' ? a += ')' : a += '('
             }
-            return '(' + helper(v) + ')' + a;
+            return '(' + solution(v) + ')' + a;
         }
-    }
-    return helper(p);
 }
