892b0038d893fedfd8324ec9da5f0de728f0269f
Lmoti <tv13579@naver.com>
2022-11-29 23:06:41 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,6 +1,5 @@
 function solution(my_string) {
     let answer = 0;
-    let str = '';
     for (let i = 0; i < my_string.length; i++) {
         if (Number(my_string[i])) answer += Number(my_string[i]);
     }
