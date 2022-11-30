5c7e9ae2661477be525900d8cfc0f8a6d7ae224a
Lmoti <tv13579@naver.com>
2022-11-30 21:22:38 +0900

    [level 0] Title: 합성수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = 0;
+    for (let i = 4; i <= n; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                answer++;
+                break;
+            }
+        }
+    }
+    return answer;
+}
