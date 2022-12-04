5fe2fa51f7981f620ae05a3e9e3c0c87ad08fd7d
Lmoti <tv13579@naver.com>
2022-12-04 16:04:14 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let answer = 0;
-    for (let i = 1; i <= n; i++) {
+    for (let i = 1; i < Math.sqrt(n); i++) {
         if (n % i === 0) answer++;
     }
-    return answer;
+    return Number.isInteger(Math.sqrt(n)) ? answer * 2 + 1 : answer * 2;
 }
