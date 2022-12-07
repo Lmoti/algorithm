a6b225b65f7e485b6c854e0fd7ab72195d00b856
Lmoti <tv13579@naver.com>
2022-12-07 18:22:17 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,5 +1,3 @@
 function solution(my_string, n) {
-    let str = '';
-    my_string.split('').map(el => str += el.repeat(n));
-    return str;
+    return my_string.split('').map(el => el.repeat(n)).join('');
 }
