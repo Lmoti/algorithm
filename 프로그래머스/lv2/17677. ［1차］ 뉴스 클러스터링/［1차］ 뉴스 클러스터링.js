8ccddddae33fe7faa92de6f689f096e078d5477c
Lmoti <tv13579@naver.com>
2022-12-12 14:41:26 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,32 @@
+function solution(str1, str2) {
+    let answer = 0;
+    const eng = /^[a-zA-Z]*$/;
+    const arr1 = [];
+    const arr2 = [];
+    let union = [];
+    const intersect = [];
+    const idx = [];
+    
+    for (let i = 0; i < str1.length - 1; i++) {
+        let j = str1[i] + str1[i + 1]
+        if (eng.test(j)) arr1.push(j.toLowerCase())
+    }
+    
+    for (let i = 0; i < str2.length - 1; i++) {
+        let j = str2[i] + str2[i + 1]
+        if (eng.test(j)) arr2.push(j.toLowerCase())
+    }
+    
+    for (let i of arr1) {
+        if (arr2.includes(i)) {
+            intersect.push(i);
+            arr2.splice(arr2.indexOf(i), 1);
+            idx.push(arr1.indexOf(i));
+            
+        }
+    }
+    
+    union = arr1.concat(arr2);
+    if (arr1.length === 0 && arr2.length === 0) return 65536;
+    return Math.floor(intersect.length / union.length * 65536);
+}
