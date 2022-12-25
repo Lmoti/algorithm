71f70daa4c777b2219735a1a6b19e4b278b6b5bd
Lmoti <tv13579@naver.com>
2022-12-25 20:23:33 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -3,5 +3,5 @@ function solution(food) {
     for (let i = 1; i < food.length; i++) {
         answer += String(i).repeat(food[i] / 2);
     }
-    return [...answer, 0, [...answer].reverse().join('')].join('');
+    return answer + 0 + [...answer].reverse().join('');
 }
