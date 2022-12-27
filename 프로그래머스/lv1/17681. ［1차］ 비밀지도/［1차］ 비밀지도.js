24396f0aa7f8d1f227e07512c73afd882bf71053
Lmoti <tv13579@naver.com>
2022-12-27 19:05:39 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,22 @@
+function solution(n, arr1, arr2) {
+    const answer = [];
+    const map = [];
+    for (let i = 0; i < n; i++) {
+        map.push([]);
+        for (let j = 0; j < n; j++) {
+            map[i].push(' ')
+        }
+    }
+    const arr11 = arr1.map((el) => ('0'.repeat(16) + el.toString(2)).slice(-n));
+    const arr22 = arr2.map((el) => ('0'.repeat(16) + el.toString(2)).slice(-n));
+    for (let i = 0; i < n; i++) {
+        const arr111 = arr11[i].split('');
+        const arr222 = arr22[i].split('');
+        for (let j = 0; j < n; j++) {
+            Number(arr111[j]) === 1 ? map[i][j] = '#' : null;
+            Number(arr222[j]) === 1 ? map[i][j] = '#' : null;
+        }
+    }
+    map.map((el) => answer.push(el.join('')));
+    return answer;
+}
