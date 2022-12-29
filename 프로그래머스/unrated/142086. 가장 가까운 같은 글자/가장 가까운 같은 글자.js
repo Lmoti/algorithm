9cdb76e3490d917ec3f8f9fec93a88edf38d2785
Lmoti <tv13579@naver.com>
2022-12-29 20:25:27 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 3.51 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,14 +1,18 @@
 function solution(s) {
-    const answer = [];
-    const obj = {};
-    for (let i = 0; i < s.length; i++) {
-        if (!obj[s[i]]) {
-            answer.push(-1);
-            obj[s[i]] = [i];
-        } else {
-            answer.push(i - obj[s[i]].at(-1));
-            obj[s[i]] = [...obj[s[i]], i];
-        }
-    }
-    return answer;
+    // const answer = [];
+    // const obj = {};
+    // for (let i = 0; i < s.length; i++) {
+    //     if (!obj[s[i]]) {
+    //         answer.push(-1);
+    //         obj[s[i]] = [i];
+    //     } else {
+    //         answer.push(i - obj[s[i]].at(-1));
+    //         obj[s[i]] = [...obj[s[i]], i];
+    //     }
+    // }
+    // return answer;
+    return [...s].map((el, i) => {
+        const count = s.slice(0, i).lastIndexOf(el);
+        return count < 0 ? count : i - count;
+    })
 }
