ed6a5372861c45d2dea344f8bcd9b5ec95e2d78f
Lmoti <tv13579@naver.com>
2023-01-04 14:24:42 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 19.97 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -0,0 +1,26 @@
+function solution(files) {
+    const obj = {};
+    for (let file of files) {
+        let head = '';
+        let number = '';
+        let tail = '';
+        let check = true;
+        for (let i = 0; i < file.length; i++) {
+            if (number === '' && (isNaN(Number(file[i])) || file[i] === ' ')) head += file[i];
+            else if (!isNaN(Number(file[i])) && check) number += file[i];
+            else {
+                check = false;
+                tail += file[i];
+            }
+        }
+        obj[file] = [head.toLowerCase(), +number, tail];
+    }
+    const arr = Object.entries(obj);
+    arr.sort((a, b) => {
+        if (a[1][0] < b[1][0]) return -1;
+        else if (a[1][0] > b[1][0]) return 1;
+        else return a[1][1] - b[1][1]
+    })
+    console.log(arr);
+    return arr.map((el) => el[0]);
+}
