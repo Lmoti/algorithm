6136bb0a7461e1234404bba67dacf327dc462322
Lmoti <tv13579@naver.com>
2023-01-23 23:26:09 +0900

    [Bronze V] Title: 윤년, Time: 184 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,6 @@
+const input = +require('fs').readFileSync('/dev/stdin').toString().trim();
+
+if (input % 4 === 0) {
+    if (input % 100 !== 0 || input % 400 === 0) console.log(1);
+    else console.log(0);
+} else console.log(0);
