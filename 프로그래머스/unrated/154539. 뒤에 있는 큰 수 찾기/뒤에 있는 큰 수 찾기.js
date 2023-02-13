7b6f9b86a7eca1daf84a45f515406c1ae04813c7
Lmoti <tv13579@naver.com>
2023-02-13 14:54:35 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 155.38 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -5,9 +5,5 @@ function solution(numbers) {
         while(stack.length && numbers[stack.at(-1)] < numbers[i]) answer[stack.pop()] = numbers[i];
         stack.push(i);
     }
-    
-    while (stack.length) {
-        answer[stack.pop()] = -1;
-    }
     return answer;
 }
