18ea2c8d178ad9424cdecde808cbbf2ceed509e5
Lmoti <tv13579@naver.com>
2023-02-19 23:31:58 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 864 ms, Memory: 69192 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.js
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const set = new Set();
+
+for (let i = 0; i < input.length; i++) {
+    for (let j = i; j < input.length; j++) {
+        set.add(input.slice(i, j + 1));
+    }
+}
+console.log(set.size);
