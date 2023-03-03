4dedb8ce99733daf0804f4948e987b33dc1e92d0
Lmoti <tv13579@naver.com>
2023-03-03 17:23:54 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.23 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949#) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 4.99 ms
+메모리: 37.9 MB, 시간: 3.23 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -4,7 +4,7 @@ function solution(arr1, arr2) {
         const tempArr = [];
         for (let i = 0; i < arr2[0].length; i++) {
             let temp = 0;
-            for (let j = 0; j < arr2.length; j++) {
+            for (let j = 0; j < row.length; j++) {
                 temp += row[j] * arr2[j][i];
             }
             tempArr.push(temp);
