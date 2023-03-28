e46ca3892127248b562f2c53864cc35baf72ba2f
Lmoti <tv13579@naver.com>
2023-03-28 19:43:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 988.97 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,15 @@
+function solution(elements) {
+    const set = new Set();
+    const len = elements.length;
+    elements.forEach((el) => {
+        if (!set.has(el)) set.add(el)
+    });
+    for (let i = 0; i < len; i++) {
+        for (let j = i + 2; j <= elements.length; j++) {
+            const sum = elements.slice(i, j).reduce((a, b) => a + b, 0);
+            if (!set.has(sum)) set.add(sum);
+        }
+        elements.push(elements[i]);
+    }
+    return set.size;
+}
